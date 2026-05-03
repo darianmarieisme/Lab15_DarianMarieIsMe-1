1a7b022f292301d49fe92eb21808a40971fb1131
Darian Bruce <darianmariebruce@gmail.com>
2026-05-03 17:37:45 -0400

    finished assignment

--- Lab15_DarianMarieIsMe-1.py
+++ Lab15_DarianMarieIsMe-1.py
@@ -3,3 +3,25 @@ Darian Marie Bruce
 This program uses a mathematical formula to plot data
 05/03/2026
 '''
+
+import matplotlib.pyplot as plt
+import math
+
+x_values: list[float] = []
+y_values: list[float] = []
+
+for degree in range(0, 361):
+    radians = math.radians(degree)
+    x_values.append(degree)
+    y_values.append(math.sin(radians))
+
+plt.plot(x_values, y_values)
+
+plt.title("Sine Wave")
+plt.xlabel("Degrees")
+plt.ylabel("sin(x)")
+
+plt.grid(True)
+
+plt.savefig("sine_wave.png")
+plt.show()
